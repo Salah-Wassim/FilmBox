01354467e433b2158bac920d40bc02a074894bb1
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-04-03 15:30:31 +0200

    Initialisation du projet et mise en place de l'API

--- App.js
+++ App.js
@@ -3,6 +3,29 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
+  let url = `https://theaudiodb.p.rapidapi.com/searchtrack.php?s=${artist}&t=${nom}`;
+  fetch(url, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "4368517609msh05c2125145348ddp123c63jsn54d49d6190df",
+      "x-rapidapi-host": "theaudiodb.p.rapidapi.com"
+    }
+  }).then(response => 
+    response.json().then(data => {
+    console.log(data);
+    //console.log(data.track[0].strAlbum);
+    const json = data.track[0];
+    document.querySelector('#music-response').innerHTML = 
+    [	
+      json.strAlbum, 
+      json.strDescriptionEN,
+      json.strGenre, 
+      json.strMood , 
+      json.strMusicVidCompany
+    ];
+  }));
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
