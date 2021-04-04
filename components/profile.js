dad2f91451f534f27d4c0d488dc41d09b12ebc36
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-04-04 12:17:08 +0200

    Importation de la librairie React, exportation d'un fonction par défaut et creation d'une constante styles

---
+++ components/profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {} from 'react-native'
+
+export default function profile(){
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
