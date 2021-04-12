4f61cd2f8535571a5e7d7dfe158578406fc4a9fc
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-04-12 15:55:59 +0200

    Création de la fonction _displayLoading() et ajout de la balise ActivityIndicator dans search.js

--- pages/search.js
+++ pages/search.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import Styles from '../Styles'
 import FilmItem from '../components/FilmItem'
-import { FlatList, StyleSheet, TextInput, View, Button } from 'react-native'
+import { FlatList, StyleSheet, TextInput, View, Button, ActivityIndicator } from 'react-native'
 
 import { getFilmsFromApiWWithSearchedText } from '../api/Api'
 
 export default class Search extends React.Component{
    constructor(props){
       super(props)
-      this.state = { films: [] }
       this.searchedText = "" 
+      this.state = { 
+         films: [],
+         isLoading: false, 
+      }
    }
 
    _loadFilms() {
-      if (this.searchedText > 0) {
+      if (this.searchedText.length > 0) {
+         this.setState({isLoading: true}) // Lancement du chargement
          getFilmsFromApiWWithSearchedText(this.searchedText).then(data => {
             console.log(data)
+            this.setState({isLoading: false}) // Fin du chargement
             this.setState({ films: data.results })
          })
       }
@@ -25,11 +30,22 @@ export default class Search extends React.Component{
       this.searchedText = text
    }
 
+   _displayLoading() {
+      if(this.state.isLoading) {
+         return(
+            <View style={styles.loading_container}>
+               <ActivityIndicator size="large"/>
+            </View>
+         )
+      }
+   }
+
    render(){
       return(
          <View>
                <TextInput placeholder="Titre du film"
                   onChangeText = {(text) => this._searchedTextInputChanged(text)}
+                  onSubmitEditing = {() => this._loadFilms()}
                />
                <Button title="rechercher" onPress={() => this._loadFilms()}/>
                <FlatList
@@ -37,11 +53,20 @@ export default class Search extends React.Component{
                   keyExtractor={(item) => item.id.toString()}
                   renderItem={({item}) => <FilmItem film={item}/>}
                />
+               {this._displayLoading()}
          </View>
       )
    }
 }
 
 const styles = StyleSheet.create({
- 
+   loading_container: {
+      position:'absolute',
+      left:0,
+      right:0,
+      top:100,
+      bottom:0,
+      alignItems:'center',
+      justifyContent:'center'
+   }
 })
