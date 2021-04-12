0d848874c6b33c19e6462104a265e669bd4c22b6
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-04-12 12:58:19 +0200

    Création d'un fichier ficheFilm.js dans le dossier pages

---
+++ pages/ficheFilm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {StyleSheet, View} from 'react-native'
+
+export default class ficheFilm extends React.Component {
+    render(){
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    
+})
