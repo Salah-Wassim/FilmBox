330a06c34dd4bf30f3cf44a4e791537609bc6883
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-04-23 12:05:10 +0200

    Creation du fichier home.js et suppression du fichier infoMusique.js

---
+++ pages/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {StyleSheet, View} from 'react-native'
+
+export default () => {
+
+    return(
+        <View>
+            <View>
+
+            </View>
+            <View>
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Styles from '../Styles'
-import {StyleSheet, View} from 'react-native'
+import {StyleSheet, Text, TextInput, View} from 'react-native'
 
 export default function profile(){
     return(
-        <View>
+        <View style={styles.profileView}>
             
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    profileView: {
 
+    }
 })
