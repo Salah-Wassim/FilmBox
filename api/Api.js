98bd47489dad57b27df4a311cfb87cbc8f7f855a
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-04-29 15:22:17 +0200

    Modification de la fonction getFilmsFromApiWWithSearchedText en ajoutant le paramètre page dans l'url

--- api/Api.js
+++ api/Api.js
@@ -1,8 +1,8 @@
 const API_TOKEN = "26baf3b79857d440c70d30245fcf0c2f"
 
-export function getFilmsFromApiWWithSearchedText (text) {
+export function getFilmsFromApiWWithSearchedText (text, page) {
     const url  = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + 
-    '&language=fr&query=' + text
+    '&language=fr&query=' + text + "&page=" +  page
     return fetch(url)
     .then(response => response.json())
     .catch(error => {console.log(error)})
