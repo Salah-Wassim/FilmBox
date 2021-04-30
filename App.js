22b4b2ae2368a6e709a6cd435846b5eda05480ae
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-04-30 11:42:14 +0200

    Suppression du import {createStackNavigator, createAppContainer} from 'react-navigation-stack'

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import {createAppContainer, createStackNavigator} from "react-navigation-stack"
 
 import Search from './pages/search'
 import SignIn from './pages/signIn'
@@ -8,20 +9,6 @@ import Home from './pages/home'
 import SignUp from './pages/registrationUser'
 
 export default function App() {
-
-  const [idPage, setIdPage] = useState('1');
-  const pages = [
-    {
-      id: "1",
-      title:"signIn",
-      component: <SignIn actif={setIdPage}/>
-    },
-    {
-      id: "2",
-      title:"signUp",
-      component: <SignUp/>
-    },
-  ]
 
   return (
       <View style={styles.container}>
