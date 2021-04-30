78b34823ec19322038aa8971859036fd195290fd
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-04-30 11:00:27 +0200

    Installation react-navigation/stack et enregistrement dans les dépendences du fichier package.json, création d'un constante SearchStackNavigator dans le fichier Navigation.js qui contient la méthode createStackNavigator()

---
+++ Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import {StyleSheet, View} from 'react-native'
+
+const SearchStackNavigator = createStackNavigator({
+
+})
+
+const styles = StyleSheet.create({
+
+})
