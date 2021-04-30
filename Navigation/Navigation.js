ab89a553bdd7944242ff89207960e1561efccef2
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-04-30 11:25:49 +0200

    Initialisation de l'écran Search

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,11 +1,13 @@
 import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
 import {StyleSheet, View} from 'react-native'
+import Search from '../pages/search'
 
 const SearchStackNavigator = createStackNavigator({
-
-})
-
-const styles = StyleSheet.create({
-
+    Search:{
+        screen: Search,
+        navigationOptions: {
+            title: 'Search'
+        }
+    }
 })
