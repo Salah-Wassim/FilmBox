97da90d82814912c80369d364f44b7be43fb45b7
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-05-01 16:06:17 +0200

    Suppression du constructor

--- components/ficheFilm.js
+++ components/ficheFilm.js
@@ -2,12 +2,6 @@ import React from 'react'
 import {Button, Image, StyleSheet, View, Text} from 'react-native'
 
 export default class ficheFilm extends React.Component {
-    
-    constructor(props){
-        super(props)
-        this.state = {
-        }
-    }
 
     render(){
         return (
