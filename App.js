d3847e7a9b0e369ec93ba42e312301bea5b8b952
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-05-01 16:02:05 +0200

    Installation des libraries react-native-gesture-handler, react-native-reanimated, react-native-safe-area-context, react-native-screens, @react-navigation/stack et @react-native-community/masked-view

--- App.js
+++ App.js
@@ -3,11 +3,6 @@ import { StyleSheet, View } from 'react-native';
 import Navigation from './Navigation/Navigation'
 
 import Search from './pages/search'
-import SignIn from './pages/signIn'
-import Profile from './pages/profile'
-import Home from './pages/home'
-import SignUp from './pages/registrationUser'
-import { render } from 'react-dom';
 
 export default class App extends React.Component {
   render(){
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,4 +1,5 @@
-import {createStackNavigator, createAppContainer} from "react-navigation-stack"
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from '@react-navigation/stack'
 
 import Search from '../pages/search'
 
