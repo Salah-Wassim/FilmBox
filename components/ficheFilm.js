f705b2d068f19ba7c153ff6a6c966bcc025427ed
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-05-05 19:52:20 +0200

    Ajout du constructeur et de la fonction _displayLoading

--- components/ficheFilm.js
+++ components/ficheFilm.js
@@ -2,6 +2,23 @@ import React from 'react'
 import {StyleSheet, View, Text} from 'react-native'
 
 export default class ficheFilm extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            film: undefined,
+            isLoading: false
+        }
+    }
+
+    _displayLoading = () => {
+        if(this.state.isLoading){
+            return(
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size="large"/>
+                </View>
+            )
+        }
+    }
 
     render(){
         console.log(this.props.route.params.id)
