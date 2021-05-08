696cf7670f5807ebacc7f704a25bbcd682027d96
Salah-Wassim ARFA <70902201+Salah-Wassim@users.noreply.github.com>
2021-05-08 17:15:47 +0200

    Importation de {createBottomTabNavigator}

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View, StatusBar} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
 import Search from './pages/search'
 import Test from './components/test'
