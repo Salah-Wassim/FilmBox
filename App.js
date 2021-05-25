c032e963df0cd1a264d6571e00fe9ca033bb4d18
Salah-Wassim <salahwassim.arfae1@gmail.com>
2021-05-25 12:46:37 +0200

    Création du dossier Security avec la création des fichier signIn.js et signUp.js

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, StatusBar, TabBarIOS} from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
 import Search from './pages/search'
 import Test from './components/test'
@@ -12,23 +11,16 @@ import Home from './pages/home'
 import Profil from './pages/profile'
 import FicheFilm from './components/ficheFilm'
 
-
 export default function App () {
 
   const Stack = createStackNavigator()
-  const Tab = createBottomTabNavigator()
-  
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Search'>
         <Stack.Screen name='Search' component={Search}/>
         <Stack.Screen name='FicheFilm' component={FicheFilm}/>
       </Stack.Navigator>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home}/>
-        <Tab.Screen name='Search' component={Search}/>
-        <Tab.Screen name='Profil' component={Profil}/>
-      </Tab.Navigator>
     </NavigationContainer>
   );
 }
